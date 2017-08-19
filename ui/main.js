6569538aa174c56e5307036ca967ffbede5eacb9
Vikram9091 <30754422+Vikram9091@users.noreply.github.com>
2017-08-19 21:17:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+
+element.innerHTML('WELCOME!!');
+  var marginleft=0;
+function marginRight(){
+    marginleft=marginleft+5;
+    img.style.marginleft=marginleft+'px';
+}
+
+var img=document.getElementById('madi');
+img.onlick=function(){
+    var interval=setInterval(marginRight,50)
+};
