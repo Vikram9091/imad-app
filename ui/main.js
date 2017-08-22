c64404029f52eae6c52cbc833ef535659cea84eb
Vikram9091 <30754422+Vikram9091@users.noreply.github.com>
2017-08-23 00:36:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,7 @@ button.onclick=function(){
  request.open('GET', 'http://viky7star.imad.hasura-app.io/counter',true);
  request.send(null);
 };
-var nameInput=document.getElementById('name');
-var names=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var request=new XMLHttpRequest();
@@ -23,17 +22,22 @@ submit.onclick=function(){
    if(request.readyState===XMLHttpRequest.DONE){
        if(request.status===200)
        {
-            var counter=request.responseText;
-            var names=['name1','name2','name3'];
+    var names=request.responseText;
+     names=JSON.parse('names');
     var list='';
     for(var i=0;i<names.length;i++)
     {
     list +='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById('namelist');
-    list.innerHTML=list;
+    ul.innerHTML=list;
        }
    }
-    
 };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    request.open('GET', 'http://viky7star.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+};
+
 
