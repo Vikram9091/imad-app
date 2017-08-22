b93eaf4fc4e4247174df52c35315cd60fb6b3607
Vikram9091 <30754422+Vikram9091@users.noreply.github.com>
2017-08-23 00:19:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,9 +15,25 @@ button.onclick=function(){
  request.send(null);
 };
 var nameInput=document.getElementById('name');
-var name=nameInput.value;
+var names=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var name
-}
+    var request=new XMLHttpRequest();
+  request.onreadystatechange=function(){
+   if(request.readyState===XMLHttpRequest.DONE){
+       if(request.status===200)
+       {
+            var counter=request.responseText;
+            var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    {
+    list +='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    list.innerHTML=list;
+       }
+   }
+    
+};
 
