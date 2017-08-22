9ae25dfc243bd23ac772d0225fee4add19bd5928
Vikram9091 <30754422+Vikram9091@users.noreply.github.com>
2017-08-22 13:25:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-var button=doucment.getElementById("counter");
+var button=doucment.getElementById('counter');
 var counter=0;
 button.onclick=function(){
   counter=counter+1;
-  var span=doucment.getElementById("count");
+  var span=doucment.getElementById('count');
   span.innerHTML=counter.toString();
 };
