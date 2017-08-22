5557962c1562ed05ea45ab3b867649d56ff2425e
Vikram9091 <30754422+Vikram9091@users.noreply.github.com>
2017-08-23 00:37:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ submit.onclick=function(){
 };
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
-    request.open('GET', 'http://viky7star.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET', 'http://viky7star.imad.hasura-app.io/submit-name?name='+names,true);
     request.send(null);
 };
 
